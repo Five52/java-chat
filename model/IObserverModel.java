ba6620aa4575934f945e50e4ae35cd004ff3606f
Thomas Fortin <contact@thomasfortin.fr>
2017-02-27 22:03:33 +0100

    Creation of observer pattern classes

---
+++ model/IObserverModel.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface IObserverModel {
+    public void modelChanged(Object src);
+}
---
+++ model/ObservableModel.java
@@ -0,0 +1,31 @@
+package dnr.model;
+
+import java.util.ArrayList;
+
+public abstract class ObservableModel {
+    private ArrayList<IObserverModel> listeners;
+
+    public ObservableModel() {
+        this(false);
+    }
+
+    public ObservableModel(boolean change) {
+        listeners = new ArrayList<IObserverModel>();
+        if (change)
+            triggerChange();
+    }
+
+    public void addObserverModel(IObserverModel iom) {
+        this.listeners.add(iom);
+    }
+
+    public void removeObserverModel(IObserverModel iom) {
+        this.listeners.remove(iom);
+    }
+
+    protected void triggerChange() {
+        for (int i = 0; int < listeners.size(); i++ {
+            listeners.get(i).modelChanged(this);
+        }
+    }
+}
